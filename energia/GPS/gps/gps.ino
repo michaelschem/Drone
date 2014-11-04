16d20ba22a616a7ec6247efae722749f3b7e87ef
michaelfschem@gmail.com <michaelfschem@gmail.com>
2014-11-03 21:06:43 -0600

    WiFi basics & GPS through Serial

---
+++ energia/GPS/gps/gps.ino
@@ -0,0 +1,24 @@
+String gpsBuffer;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  probeGPS();
+}
+
+void probeGPS(){
+  if(Serial.available()) {
+    char next = Serial.read();
+    if(next != '\n'){
+      gpsBuffer += next;
+    } else {
+      if(gpsBuffer.startsWith("$GPGLL")){
+        Serial.println(gpsBuffer);
+      }
+      gpsBuffer = "";
+    }
+  }
+}
+
