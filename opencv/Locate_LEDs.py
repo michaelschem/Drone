42341acc85f0f46a8245e2739667b4f444905ee8
Nathan Moore <moorednathan@gmail.com>
2014-11-30 18:57:00 -0600

    Beginning to add beacon identification to the basic detection algorithm

--- opencv/Locate_LEDs.py
+++ opencv/Locate_LEDs.py
@@ -6,6 +6,17 @@ vid_file = 'Videos\\IR_DARK_FILTERED.mp4'
 cap = cv2.VideoCapture(vid_file)    # the capture
 cap.open(vid_file)  # doesn't always open automatically
 frame_count = 0
+frames_total = int(cap.get(cv.CV_CAP_PROP_FRAME_COUNT))
+fps = int(cap.get(cv.CV_CAP_PROP_FPS))
+frame_width = cap.get(cv.CV_CAP_PROP_FRAME_WIDTH)
+lefty = [0] * frames_total # keep track of when the left light is seen
+righty = [0] * frames_total # keep track of when the right light is seen
+led_centers = []
+one = two = zero = 0
+print 'FPS: '+str(fps)+'\n'
+print 'Total Frames: '+str(frames_total)+'\n\n'
+
+"""Find LEDs"""
 
 while(cap.isOpened()):
     # success is a boolean that lets us know if anything was returned
@@ -23,17 +34,25 @@ while(cap.isOpened()):
                                                cv2.CHAIN_APPROX_SIMPLE)
         centers = []
         radii = []
-        for cnt in contours:
-          (x,y),radius = cv2.minEnclosingCircle(cnt)
-          centers.append((int(x),int(y)))
-          radii.append(int(radius))
+        num_contours = len(contours)
+        for cntr in contours:
+            (x,y),radius = cv2.minEnclosingCircle(cntr)
+            centers.append((int(x),int(y)))
+            led_centers.append((int(x),int(y),num_contours))
+            radii.append(int(radius))
+        if num_contours == 1:
+            one += 1
+        elif num_contours == 2:
+            two += 1
+        else:
+            zero += 1
         # draw circles around LEDs in image
         size = len(centers)
         for i in range(0,size):
             cv2.circle(frame,centers[i],radii[i],(255,255,255),2)
         cv2.imshow('frames', frame)
-        # slow down the video playback
-        cv2.waitKey(10)
+        # # slow down the video playback
+        # cv2.waitKey(10)
         # lets us quit the video early
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -44,3 +63,36 @@ while(cap.isOpened()):
 cap.release()
 # out.release()
 cv2.destroyAllWindows()
+
+"""Determine how often each LED was seen"""
+
+# num_centers = len(led_centers)
+# for center in range(0,num_centers):
+#     if led_centers[center][2] == 1:
+
+lefty_freq = 0
+righty_freq = 0
+
+for i in range(0,frames_total):
+    if lefty[i] == 1:
+        lefty_freq += 1
+    if righty[i] == 1:
+        righty_freq += 1
+
+print "Two frequency: "+str(two)+"\n"
+print "One frequency: "+str(one)+"\n"
+print "Zero frequency: "+str(zero)+"\n"
+
+# lefty_freq = (float(lefty_freq)/float(frames_total)) * 100.0
+# righty_freq = (float(righty_freq)/float(frames_total)) * 100.0
+
+two = (float(two)/float(frames_total)) * 100.0
+one = (float(one)/float(frames_total)) * 100.0
+zero = (float(zero)/float(frames_total)) * 100.0
+
+# lefty_freq = float(lefty_freq/frames_total) * 100.0
+# righty_freq = float(righty_freq/frames_total) * 100.0
+
+print "Two frequency: "+str(two)+"\n"
+print "One frequency: "+str(one)+"\n"
+print "Zero frequency: "+str(zero)+"\n"
--- opencv/vidCapture.py
+++ opencv/vidCapture.py
@@ -1,64 +1,66 @@
-import numpy as np
-import cv2
-import cv2.cv as cv
+# DONT RUN #
 
-def findCircles(img):
-    """Find circles in an image"""
-    # blur the image first to reduce noise
-    blur = cv2.GaussianBlur(img, (9,9), 0)
-    # calculate threshold
-    ret,thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
-    # find circles
-    rows,cols = thresh.shape
-    circles = cv2.HoughCircles(thresh, cv.CV_HOUGH_GRADIENT, 1, rows/8,
-                               param1=200, param2=100, minRadius=0,
-                               maxRadius=0)
-    return circles
+# import numpy as np
+# import cv2
+# import cv2.cv as cv
 
-def drawCircles(img, circles):
-    """Draw the circles on an image after circle detection"""
-    for i in circles[0,:]:
-        # draw the outer circle
-        cv2.circle(img, (i[0],i[1]), i[2], (0,255,0), 2)
-        # draw the center of the circle
-        cv2.circle(img, (i[0],i[1]), 2, (0,0,255), 3)
-    return img
+# def findCircles(img):
+#     """Find circles in an image"""
+#     # blur the image first to reduce noise
+#     blur = cv2.GaussianBlur(img, (9,9), 0)
+#     # calculate threshold
+#     ret,thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
+#     # find circles
+#     rows,cols = thresh.shape
+#     circles = cv2.HoughCircles(thresh, cv.CV_HOUGH_GRADIENT, 1, rows/8,
+#                                param1=200, param2=100, minRadius=0,
+#                                maxRadius=0)
+#     return circles
 
-vid_file = 'Videos\\IR_DARK_FILTERED.mp4'
-cap = cv2.VideoCapture(vid_file)    # the capture
-cap.open(vid_file)  # doesn't always open automatically
+# def drawCircles(img, circles):
+#     """Draw the circles on an image after circle detection"""
+#     for i in circles[0,:]:
+#         # draw the outer circle
+#         cv2.circle(img, (i[0],i[1]), i[2], (0,255,0), 2)
+#         # draw the center of the circle
+#         cv2.circle(img, (i[0],i[1]), 2, (0,0,255), 3)
+#     return img
 
-# fourcc = cv.CV_FOURCC(*'XVID')
-# out = cv2.VideoWriter('Videos\\output.avi',fourcc, 20.0, (1920,1080))
+# vid_file = 'Videos\\IR_DARK_FILTERED.mp4'
+# cap = cv2.VideoCapture(vid_file)    # the capture
+# cap.open(vid_file)  # doesn't always open automatically
 
-while(cap.isOpened()):
-    # ret is a boolean that lets us know if anything was returned
-    ret, frame = cap.read()
-    if ret==True:
-        # if a frame was returned; make frame grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        circles = findCircles(gray)
-        # draw circles
-        if type(circles) is np.ndarray:
-            # if any LEDs detected, they will be of data type numpy.ndarray
-            circles = np.uint16(np.around(circles))
-            gray = drawCircles(gray, circles)
-        # Show when a circle was detected and where so that we can adjust the
-        # param1 and param2 of HoughCircles as needed.
-        cv2.imshow('frames', gray)
-        # slow down the video playback
-        cv2.waitKey(10)
-        # lets us quit the video early
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        # out.write(gray)
-    else:
-        break
+# # fourcc = cv.CV_FOURCC(*'XVID')
+# # out = cv2.VideoWriter('Videos\\output.avi',fourcc, 20.0, (1920,1080))
 
-# When everything done, release the capture
-cap.release()
-# out.release()
-cv2.destroyAllWindows()
+# while(cap.isOpened()):
+#     # ret is a boolean that lets us know if anything was returned
+#     ret, frame = cap.read()
+#     if ret==True:
+#         # if a frame was returned; make frame grayscale
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#         circles = findCircles(gray)
+#         # draw circles
+#         if type(circles) is np.ndarray:
+#             # if any LEDs detected, they will be of data type numpy.ndarray
+#             circles = np.uint16(np.around(circles))
+#             gray = drawCircles(gray, circles)
+#         # Show when a circle was detected and where so that we can adjust the
+#         # param1 and param2 of HoughCircles as needed.
+#         cv2.imshow('frames', gray)
+#         # slow down the video playback
+#         cv2.waitKey(10)
+#         # lets us quit the video early
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#         # out.write(gray)
+#     else:
+#         break
+
+# # When everything done, release the capture
+# cap.release()
+# # out.release()
+# cv2.destroyAllWindows()
 
 
 
