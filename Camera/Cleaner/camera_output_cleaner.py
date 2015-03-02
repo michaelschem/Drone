c90b36ff029657db3ce29e3156885d98e1eeebb8
Nathan Moore <moorednathan@gmail.com>
2015-03-02 13:35:17 -0600

    Updated the cleaner script so that it outputs actual HEX instead of ASCII or UTF-8 encoded chars. Now have jpegs and bin files in the clean folder. Also found that 1.txt may not be properly formatted to be a jpeg.

--- Camera/Cleaner/camera_output_cleaner.py
+++ Camera/Cleaner/camera_output_cleaner.py
@@ -1,38 +1,37 @@
-"""Cleans the weird camera output files we keep getting"""
+"""Cleans the weird camera output files we keep getting and converts to hex"""
 
-import re, os, glob
+import re, os, glob, array
 
 __out_num__ = 0
+
+# convert to actual hex
+def string_to_hex(string_hex):
+
+    hex_string = string_hex.decode('hex')
+    hex_string = array.array('B', hex_string)
+    cleaned_data_output.write(hex_string)
 
 # read files from directory "Unclean" which is in this python script's root, too
 # Ex: Dir/camera_output_cleaner.py and Dir/Unclean/...
 for filename in glob.iglob(os.path.join('Unclean', '*.txt')):
-    with open(filename, 'r') as f:
+    with open(filename, 'rb') as f:
         # open file as read only
         camera_data_text = f.read()
+        print filename
         # open file as write only
         __out_num__ += 1
         # output cleaned files to this directory
-        out_file = 'Clean/clean2_'+str(__out_num__)+'.txt'
-        cleaned_data_output = open(out_file, 'w')
-
-        # replace V2 with space
-        regex = re.compile('[Vv]')
-        camera_data_text = regex.sub(' ', camera_data_text)
+        out_file = 'Clean/clean2_'+str(__out_num__)+'.bin'
+        cleaned_data_output = open(out_file, 'wb')
 
         # remove any strings that clearly aren't hex
-        regex = re.compile('[^ABCDEF\d\n]{0,}')
+        regex = re.compile('[^ABCDEF0123456789\n]{0,}')
         camera_data_text = regex.sub('', camera_data_text)
 
         # remove all other random characters
-        regex = re.compile('([ABCDEF\d]{113})')
+        regex = re.compile('([ABCDEF\d]{112})')
         newlist = re.findall(regex,camera_data_text)
 
-        # # reinsert spaces to show each byte
-        # camera_data_text = re.sub('([\d\w]{2})','\\1 ',camera_data_text)
-
-        # string_to_hex(camera_data_text)
         for item in newlist:
-            print item
-            cleaned_data_output.write(item)
+            item = string_to_hex(item)
 
